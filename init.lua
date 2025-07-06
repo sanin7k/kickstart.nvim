88219fef5ba1e458116de299f6f6b9c61b54e119
Sanin K <dev.sanink@gmail.com>
2025-07-06 08:05:00 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -245,7 +245,7 @@ rtp:prepend(lazypath)
 --    :Lazy update
 --
 -- NOTE: Here is where you install your plugins.
-require('lazy').setup(sources = {
+require('lazy').setup({sources = {
     providers = {
       cmdline = {
         -- ignores cmdline completions when executing shell commands
@@ -254,7 +254,7 @@ require('lazy').setup(sources = {
         end
       }
     }
-  }, {
+  }}, {
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
