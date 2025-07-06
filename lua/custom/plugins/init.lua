883b63d9db50b9c4b65c241a657129a0553712b5
Sanin K <dev.sanink@gmail.com>
2025-07-06 07:58:17 +0530

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,15 +2,4 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  sources = {
-    providers = {
-      cmdline = {
-        -- ignores cmdline completions when executing shell commands
-        enabled = function()
-          return vim.fn.getcmdtype() ~= ':' or not vim.fn.getcmdline():match("^[%%0-9,'<>%-]*!")
-        end
-      }
-    }
-  }
-}
+return {}
